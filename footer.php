89b0259d6c62ec33c1a1893783c52a3694b3211f
foadsadr <foads73@gmail.com>
2018-11-27 17:02:31 +0330

    test

---
+++ footer.php
@@ -0,0 +1,9 @@
+    <div class="clear"></div>
+    
+    <?php eval("?>".base64_decode("PGRpdiBpZD0iZm9vdGVyIj4NCiAgICAgICAgPGg2IGNsYXNzPSJoYW15YXJ3cCI+IDx0aCBjbGFzcz0iZGlzaW4iPiYjMTU4NTsmIzE1NzU7JiMxNjA3OyAmIzE1NzU7JiMxNjA2OyYjMTU4MzsmIzE1NzU7JiMxNTg2OyYjMTc0MDsgJiMxNTg4OyYjMTU4MzsmIzE2MDc7ICYjMTU3ODsmIzE2MDg7JiMxNTg3OyYjMTU5MTs8L3RoPiA8YSBocmVmPSJodHRwOi8vd3d3LmhhbXlhcndwLmNvbS8idGFyZ2V0PSJfYmxhbmsiPiYjMTYwNzsmIzE2MDU7JiMxNzQwOyYjMTU3NTsmIzE1ODU7ICYjMTYwODsmIzE1ODU7JiMxNTgzOyYjMTY2MjsmIzE1ODU7JiMxNTg3OzwvYT48L2g2Pg0KICAgIDwvZGl2Pg==")); ?><!--//footer-->
+
+</div><!--//main_container-->
+
+<?php wp_footer(); ?>
+</body>
+</html>
